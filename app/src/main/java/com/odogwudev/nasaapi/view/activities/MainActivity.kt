b96cb0c58cf5b810cc68ea3957d76258f12b4137
ghostadventures <47299656+ghostadventures@users.noreply.github.com>
2021-03-14 18:56:34 +0100

    MainActivitySetUp

---
+++ app/src/main/java/com/odogwudev/nasaapi/view/activities/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.odogwudev.nasaapi.view.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.odogwudev.nasaapi.R
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val viewPager = main_viewpager
+        val mainPagerAdapter = MainPagerAdapter.Builder(supportFragmentManager)
+            .addFragment("DailyPost", DailyPostFragment())
+            .addFragment("Asteroids", AsteroidsFragment())
+            .create()
+        viewPager.adapter = mainPagerAdapter
+        val tabLayout = main_tab_layout
+        tabLayout.setupWithViewPager(viewPager)
+    }
+}
