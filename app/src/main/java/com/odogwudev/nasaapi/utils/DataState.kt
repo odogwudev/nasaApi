dac2eca691999dc5c5c073607859375976abafec
ghostadventures <47299656+ghostadventures@users.noreply.github.com>
2021-03-14 19:50:08 +0100

    Repository Correction

--- app/src/main/java/com/odogwudev/nasaapi/utils/DataState.kt
+++ app/src/main/java/com/odogwudev/nasaapi/utils/DataState.kt
@@ -3,7 +3,7 @@ package com.odogwudev.nasaapi.utils
 import java.lang.Exception
 
 sealed class DataState<out R> {
-    data class Sucess<out T>(val data: T) : DataState<T>()
+    data class Success<out T>(val data: T) : DataState<T>()
     data class Error(val exception: Exception) : DataState<Nothing>()
 
     object Loading : DataState<Nothing>()
---
+++ app/src/main/java/com/odogwudev/repository/MainRepository.kt
@@ -0,0 +1,43 @@
+package com.odogwudev.repository
+
+import android.content.Context
+import android.util.Log
+import com.odogwudev.nasaapi.R
+import com.odogwudev.nasaapi.model.DailyPost
+import com.odogwudev.nasaapi.utils.DataState
+import com.odogwudev.repository.retrofit.NetworkService
+import com.odogwudev.repository.retrofit.dailypost.DailyPostNetworkMapper
+import com.odogwudev.repository.room.DatabaseDao
+import com.odogwudev.repository.room.dailypost.DailyPostCacheMapper
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class MainRepository constructor(
+    @ApplicationContext
+    private val context: Context,
+    private val databaseDao: DatabaseDao,
+    private val networkService: NetworkService,
+    private val dailyPostCacheMapper: DailyPostCacheMapper,
+    private val dailyPostNetworkMapper: DailyPostNetworkMapper
+) {
+    suspend fun getDailyPost(): Flow<DataState<DailyPost>> = flow {
+        emit(DataState.Loading)
+        try {
+            val networkDailyPost =
+                networkService.getDailyPost(context.getString(R.string.nasa_api_key))
+            Log.d(TAG, "getDailyPost: $networkDailyPost")
+            val dailyPost = dailyPostNetworkMapper.mapFromEntity(networkDailyPost)
+            databaseDao.insertDailyPost(dailyPostCacheMapper.mapToEntity(dailyPost))
+            val insertedDailyPost = databaseDao.get(dailyPost.title)
+            Log.d(TAG, "getDailyPost: $insertedDailyPost")
+            emit(DataState.Success(dailyPost))
+        } catch (ex: Exception) {
+            emit(DataState.Error(ex))
+        }
+    }
+
+    companion object {
+        const val TAG = "MainRepository"
+    }
+}
