98e0a8b6386a2b8db288c1674b470282e6d04991
ghostadventures <47299656+ghostadventures@users.noreply.github.com>
2021-03-14 20:28:55 +0100

    Dependency Injection Repository Module

--- app/src/main/java/com/odogwudev/nasaapi/di/RepositoryModule.kt
+++ app/src/main/java/com/odogwudev/nasaapi/di/RepositoryModule.kt
@@ -1,4 +1,39 @@
 package com.odogwudev.nasaapi.di
 
+import android.content.Context
+import com.odogwudev.repository.MainRepository
+import com.odogwudev.repository.retrofit.NetworkService
+import com.odogwudev.repository.retrofit.dailypost.DailyPostNetworkMapper
+import com.odogwudev.repository.room.DatabaseDao
+import com.odogwudev.repository.room.dailypost.DailyPostCacheMapper
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(ApplicationComponent::class)
 class RepositoryModule {
+
+    @Provides
+    @Singleton
+    fun provideMainRepository(
+        @ApplicationContext
+        context: Context,
+        databaseDao: DatabaseDao,
+        networkService: NetworkService,
+        dailyPostCacheMapper: DailyPostCacheMapper,
+        dailyPostNetworkMapper: DailyPostNetworkMapper
+    ): MainRepository {
+        return MainRepository(
+            context,
+            databaseDao,
+            networkService,
+            dailyPostCacheMapper,
+            dailyPostNetworkMapper
+        )
+    }
 }
